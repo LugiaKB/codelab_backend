58bfa8dd7b48f0f8d96954a3a84a30f33c021fc6
Iury Mikael <mikaeliury@gmail.com>
2025-02-06 13:52:15 -0300

    feat: create files for deployment with render

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pipenv install --deploy
+
+pipenv shell
+
+# Apply any outstanding database migrations
+pipenv run python manage.py migrate
--- codelab_backend/settings/production.py
+++ codelab_backend/settings/production.py
@@ -1,3 +1,11 @@
+import dj_database_url
+
 from codelab_backend.settings.base import *
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
