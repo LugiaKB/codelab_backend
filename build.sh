5c469c2134e27f0f6ca6c8f27de11c9d2e1c05bd
Iury Mikael <mikaeliury@gmail.com>
2025-02-07 00:48:17 -0300

    fix: fixed envVars on yaml file

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pipenv install --deploy
-
-pipenv shell
+pipenv install --deploy --ignore-pipfile
 
 # Apply any outstanding database migrations
 pipenv run python manage.py migrate
