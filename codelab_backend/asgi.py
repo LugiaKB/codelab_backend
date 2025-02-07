4216f391ed7e516f50f4413f7f3e8e9c40394c92
Iury Mikael <mikaeliury@gmail.com>
2025-02-07 01:25:45 -0300

    fix: fix asgi files to use modular settings

--- codelab_backend/asgi.py
+++ codelab_backend/asgi.py
@@ -11,6 +11,16 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codelab_backend.settings')
+from codelab_backend.settings import base as base_settings
+
+settings_definitions_based_on_debug = {
+        True: 'codelab_backend.settings.local',
+        False: 'codelab_backend.settings.production',
+    }
+
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 
+    settings_definitions_based_on_debug[base_settings.DEBUG]
+)
 
 application = get_asgi_application()
