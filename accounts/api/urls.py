0aef1f80e720ecec8705c5a8f0932c4a37a0daaf
Iury Mikael <mikaeliury@gmail.com>
2025-02-07 04:18:56 -0300

    feat: add swagger documentation

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -6,6 +7,7 @@ from rest_framework_simplejwt.views import (
 from accounts.api.views import SignUpView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='signup/', permanent=False), name='root'),
     path('signup/', SignUpView.as_view(), name='signup'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- codelab_backend/settings/base.py
+++ codelab_backend/settings/base.py
@@ -104,3 +104,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Codelab API",
+    "DESCRIPTION": "API documentation for Codelab project",
+    "VERSION": "1.0.0",
+}
