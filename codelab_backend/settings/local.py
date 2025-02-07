bf7595633cda841a5e5f7b2f49214b6f0651ddb4
Iury Mikael <mikaeliury@gmail.com>
2025-02-07 01:20:35 -0300

    fix: use postgres db on local settings

--- codelab_backend/settings/local.py
+++ codelab_backend/settings/local.py
@@ -1,8 +1,11 @@
+import dj_database_url
+
 from codelab_backend.settings.base import *
 
+ALLOWED_HOSTS = ['*']
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
